deb74d7766845a8991e53c3e861bc53fe23ca588
cmdr-WaschBaer <rene-hippe@live.de>
2019-05-13 03:05:54 +0200

    added better commenting

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,14 @@
+// prevent running the effects on a screen that is not wide enough
 if (document.documentElement.clientWidth > 768) {
     // init controller
     var controller = new ScrollMagic.Controller();
 
-    // Parallax background
-    new ScrollMagic.Scene({
-        triggerElement: "#parallax",
-        triggerHook: "onEnter",
+    /////////////////////////
+    // Parallax background //
+    /////////////////////////
+    new ScrollMagic.Scene({ // creating a scene for the parallax effects
+        triggerElement: "#parallax", // triggering on the id for parallax
+        triggerHook: "onEnter", // and only on entering the div with said ID
     })
         .duration('200%')
         .setTween("#parallax", {
@@ -16,12 +19,12 @@ if (document.documentElement.clientWidth > 768) {
         .addTo(controller);
 
 
-    //////////////////////
-    // Slide in Scene 1 //
-    //////////////////////
-    $(".slide").each(function() {
-        new ScrollMagic.Scene({
-            triggerElement: this,
+    /////////////////////
+    // Slide in Scenes //
+    /////////////////////
+    $(".slide").each(function() {  // runs as soon as .slide is visible in window
+        new ScrollMagic.Scene({ // creating a new scene for the parallax slide effects
+            triggerElement: this, // for current highlighted div
             triggerHook: "onLeave",
         })
             .setPin(this)
@@ -34,19 +37,22 @@ if (document.documentElement.clientWidth > 768) {
     // fadeIn from the bottom for story sections //
     ///////////////////////////////////////////////
     $(".fadeup").each(function() { // runs as soon as .fadeup is visible in window
-        var tween = TweenMax.from(this, .6, {
+        var timeline = new TimelineMax
+        var tween = TweenMax.from(this,1.5, {
             y: 100,  // offset of starting position for animation
             autoAlpha: 0, // setting the opacity to 0 for start
-            delay: 0,
             ease: Power2.easeOut
         }, .1);
+
+        timeline
+            .add(tween);
 
         new ScrollMagic.Scene({ // creating a Scene for the animation
                 triggerElement: this, // triggering when current div is reached
                 offset: -100, // offsetting the trigger point
                 reverse:true
             })
-                .setTween(tween)
+                .setTween(timeline)
                 .addTo(controller)
                 //.addIndicators() // for debugging only !
         ;
