f5af07f6991a2ca63b8aea73df2061582eb25aa6
Mathias Bethge <bethge.netz@googlemail.com>
2019-05-25 20:31:15 +0200

    add parallax, fadeIn and -Out for scene02

--- javascript/main.js
+++ javascript/main.js
@@ -3,39 +3,39 @@ $(document).ready(function () {
     // Single scrollmagic controller for the entire experience
     var controller = new ScrollMagic.Controller();
 
-$('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in view
+    $('.typewriter').each(function () { // gets called as soon as #Slide1_1 is in view
         // letter animation
         var tween = new TimelineMax(); // timeline for the typing and cursor animation
         tween.add( // adds the animation to our timeline
             TweenMax.fromTo(".anim-typewriter", 1.75,
                 {width: "0",},
-                {width: "2.5em",ease: SteppedEase.config(11)},0));
+                {width: "2.5em", ease: SteppedEase.config(11)}, 0));
         // text cursor animation
         tween.add( // adds the animation to our timeline
             TweenMax.fromTo(".anim-typewriter", 0.5,
                 {"border-right-color": "rgba(255,255,255,0.75)"},
-                {"border-right-color": "rgba(255,255,255,0)",repeat: -1,ease: SteppedEase.config(11)},0));
-    new ScrollMagic.Scene({
-        triggerElement: '.typewriter', // trigger for the start of the animation
-        triggerHook: 0.5, // start y !  for use add this to animating div data-hook=" "
-        offset: 600 // start y !  for use add this to animating div data-offset=" "
-    })
-        .setTween(tween)
-        //.addIndicators({name: "typewriter", colorTrigger: "orange", colorStart: "yellow", colorEnd: "teal"})
-        .addTo(controller);
-});
+                {"border-right-color": "rgba(255,255,255,0)", repeat: -1, ease: SteppedEase.config(11)}, 0));
+        new ScrollMagic.Scene({
+            triggerElement: '.typewriter', // trigger for the start of the animation
+            triggerHook: 0.5, // start y !  for use add this to animating div data-hook=" "
+            offset: 600 // start y !  for use add this to animating div data-offset=" "
+        })
+            .setTween(tween)
+            //.addIndicators({name: "typewriter", colorTrigger: "orange", colorStart: "yellow", colorEnd: "teal"})
+            .addTo(controller);
+    });
 
 
     //  Cover ================================
 // animationen hier sind nur platzhalter !!! da noch kein cover gebaut wurde
     var action01 = new TimelineMax()
-        .to('.title #mainTitle',20,{autoAlpha:1,y:-10},"+=4")
-        .to('.title #quote',20,{autoAlpha:1, y:-10},"+=20")
-        .to('.title #description',20,{autoAlpha:1,y:-10,delay:25},"+=20");
+        .to('.title #mainTitle', 20, {autoAlpha: 1, y: -10}, "+=4")
+        .to('.title #quote', 20, {autoAlpha: 1, y: -10}, "+=20")
+        .to('.title #description', 20, {autoAlpha: 1, y: -10, delay: 25}, "+=20");
 
     var cover = new ScrollMagic.Scene({
         triggerElement: ".cover",
-        duration:1000,
+        duration: 1000,
         triggerHook: 0.5, //  top of viewport
         reverse: true
     })
@@ -47,14 +47,14 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
     //  Intro ================================
 
     var action01 = new TimelineMax()
-        .to('.intro #mainTitle',20,{autoAlpha:1,y:-100},"+=4")
-        .to('.intro #description',20,{autoAlpha:1,y:-100},"+=8")
-        .to('.intro #quote',20,{autoAlpha:1, y:-100},"+=8")
-        .to('.intro #introscene',20,{autoAlpha:0},"+=4");
+        .to('.intro #mainTitle', 20, {autoAlpha: 1, y: -100}, "+=4")
+        .to('.intro #description', 20, {autoAlpha: 1, y: -100}, "+=8")
+        .to('.intro #quote', 20, {autoAlpha: 1, y: -100}, "+=8")
+        .to('.intro #introscene', 20, {autoAlpha: 0}, "+=4");
 
     var intro = new ScrollMagic.Scene({
         triggerElement: ".intro",
-        duration:5000,
+        duration: 5000,
         triggerHook: 0, //  top of viewport
         reverse: true
     })
@@ -68,7 +68,7 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
     // NO ANIMATION HERE SINCE ITS UP TOP INSIDE THE .each LOOP
     var prolog = new ScrollMagic.Scene({
         triggerElement: ".typewriter",
-        duration:2500,
+        duration: 2500,
         triggerHook: 0, //  top of viewport
         reverse: true
     })
@@ -80,20 +80,20 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
 // bsp: bewege nach oder mache in richtung .to( klasse , länge, {styles},
 // start versatz ( verschiebt realtiv alle animationen dahinter!)
     var action01 = new TimelineMax()
-        .to('.prolog #prolog1',10,{autoAlpha:1,y:100},"+=2") // fade from top
-        .to('.prolog #prolog1',10,{autoAlpha:0,y:-250},"+=4")// fade away to top
-        .to('.prolog #prolog2',10,{autoAlpha:1,y:-250},"-=8") // fade in from bottom
-        .to('.prolog #prolog2',10,{autoAlpha:0},"+=4")
-        .to('.prolog #prolog3',10,{autoAlpha:1},"+=4")
-        .to('.prolog #prolog3',10,{autoAlpha:0},"+=4")
-        .to('#prologQuote',10,{autoAlpha:1},"+=4")
-        .to('#prologQuote',10,{autoAlpha:0},"+=4")
-        .to('.prolog #prolog4',10,{autoAlpha:1,y:-250},"+=4") // fade from bottom
-        .to('.prolog #prolog4',10,{autoAlpha:0},"+=4") // fade from top
+        .to('.prolog #prolog1', 10, {autoAlpha: 1, y: 100}, "+=2") // fade from top
+        .to('.prolog #prolog1', 10, {autoAlpha: 0, y: -250}, "+=4")// fade away to top
+        .to('.prolog #prolog2', 10, {autoAlpha: 1, y: -250}, "-=8") // fade in from bottom
+        .to('.prolog #prolog2', 10, {autoAlpha: 0}, "+=4")
+        .to('.prolog #prolog3', 10, {autoAlpha: 1}, "+=4")
+        .to('.prolog #prolog3', 10, {autoAlpha: 0}, "+=4")
+        .to('#prologQuote', 10, {autoAlpha: 1}, "+=4")
+        .to('#prologQuote', 10, {autoAlpha: 0}, "+=4")
+        .to('.prolog #prolog4', 10, {autoAlpha: 1, y: -250}, "+=4") // fade from bottom
+        .to('.prolog #prolog4', 10, {autoAlpha: 0}, "+=4") // fade from top
     ;
-    var prolog= new ScrollMagic.Scene({
+    var prolog = new ScrollMagic.Scene({
         triggerElement: ".prolog",
-        duration:15000,
+        duration: 15000,
         triggerHook: 0, //  top of viewport
         reverse: true
     })
@@ -123,23 +123,23 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
 // für den aufbau im html dokument einfach immer an den anderen scenen orientieren
 // fade ins und fadeout müssen extra angefügt werden ! siehe unten !
     var action01 = new TimelineMax()
-            // Klasse mit ID        //dauer//  attribute   // startDelay
-        .to('.scene01 #scene_one_quinn',10,{autoAlpha:1,x:100},"+=4")
-        .to('.scene01 #scene01_bubble1',10,{autoAlpha:1,x:100},"+=2")
-        .to('.scene01 #scene01_bubble_quinn',10,{autoAlpha:1,x:100},"+=4")
-        .add("Parallax","+=15") //insert point Parallax into timeline
-        .to('.scene01 #scene_one_second_layer',15,{y:-25},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene_one_third_layer',15,{y:-22},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene_one_fourth_layer',15,{y:-18},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene_one_fifth_layer',15,{y:-16},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene_one_quinn',15,{y:-22},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene01_bubble1',15,{y:-25},"Parallax")// starting at point Parallax
-        .to('.scene01 #scene01_bubble_quinn',15,{y:-25},"Parallax")// starting at point Parallax
+        // Klasse mit ID        //dauer//  attribute   // startDelay
+            .to('.scene01 #scene_one_quinn', 10, {autoAlpha: 1, x: 100}, "+=4")
+            .to('.scene01 #scene01_bubble1', 10, {autoAlpha: 1, x: 100}, "+=2")
+            .to('.scene01 #scene01_bubble_quinn', 10, {autoAlpha: 1, x: 100}, "+=4")
+            .add("Parallax", "+=15") //insert point Parallax into timeline
+            .to('.scene01 #scene_one_second_layer', 15, {y: -25}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene_one_third_layer', 15, {y: -22}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene_one_fourth_layer', 15, {y: -18}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene_one_fifth_layer', 15, {y: -16}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene_one_quinn', 15, {y: -22}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene01_bubble1', 15, {y: -25}, "Parallax")// starting at point Parallax
+            .to('.scene01 #scene01_bubble_quinn', 15, {y: -25}, "Parallax")// starting at point Parallax
     ;
 
     var scene01 = new ScrollMagic.Scene({
         triggerElement: ".scene01",
-        duration:8000,
+        duration: 8000,
         triggerHook: 0, //  top of viewport
         reverse: true
     })
@@ -152,7 +152,7 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
 // fade in ist einfach nur die scenen klasse!
 //  FadeInScene01 ================================
     $(".scene01").each(function () {
-        var quinn3 = TweenMax.fromTo($(this),2,{opacity:0,},{opacity:1,ease:Power1.easeIn,});
+        var quinn3 = TweenMax.fromTo($(this), 2, {opacity: 0,}, {opacity: 1, ease: Power1.easeIn});
         new ScrollMagic.Scene({
             triggerElement: '.scene01',
             duration: 500,
@@ -167,7 +167,7 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
 // fadeout hingegen ist scenen klasse aber mit trigger auf der nächsten scene bei einem onEnter Hook !!
 //  FadeOutScene01 ================================
     $(".scene01").each(function () {
-        var quinn2 = TweenMax.to($(this),2,{opacity:0,ease:Power1.easeIn,});
+        var quinn2 = TweenMax.to($(this), 2, {opacity: 0, ease: Power1.easeIn,});
         new ScrollMagic.Scene({
             triggerElement: '.scene02',
             duration: 500,
@@ -177,4 +177,54 @@ $('.typewriter').each(function() { // gets called as soon as #Slide1_1 is in vie
             //.addIndicators({name: "TEST", colorStart: "orange", colorEnd: "orange",colorTrigger:"white"})
             .addTo(controller);
     });
+
+    // BEGINN SZENE02 //
+    //  //  //  //   //
+
+
+//  FadeInScene02 ================================
+    $(".scene02").each(function () {
+        var scene2 = TweenMax.fromTo($(this), 2, {opacity: 0,}, {opacity: 1, ease: Power1.easeIn});
+        new ScrollMagic.Scene({
+            triggerElement: '.scene02',
+            duration: 500,
+            triggerHook: .6,
+            offset: 0
+        })
+            .setTween(scene2)
+            //.addIndicators({name: "TEST", colorStart: "orange", colorEnd: "orange"})
+            .addTo(controller);
+    });
+    //  END FadeInScene02 ================================
+
+//  FadeOutScene02 ================================
+    $(".scene02").each(function () {
+        var scene02 = TweenMax.to($(this), 2, {opacity: 0, ease: Power1.easeIn,});
+        new ScrollMagic.Scene({
+            triggerElement: '.scene03',
+            duration: 700,
+            triggerHook: "onEnter",
+        })
+            .setTween(scene02)
+            //.addIndicators({name: "TEST", colorStart: "orange", colorEnd: "orange",colorTrigger:"white"})
+            .addTo(controller);
+    });
+    //  END FadeOutScene02 ================================
+
+    var scene02 = new TimelineMax()
+        // Klasse mit ID        //dauer//  attribute   // startDelay
+            .add("Parallax", "+=40") //insert point Parallax into timeline
+            .to('.scene02 #scene2_foreground', 15, {y: -30}, "Parallax")// starting at point Parallax
+            .to('.scene02 #scene02_quinn', 15, {y: -15}, "Parallax")
+    ;
+    new ScrollMagic.Scene({
+        triggerElement: ".scene02",
+        duration: 8000,
+        triggerHook: 0, //  top of viewport
+        reverse: true
+    })
+        .setTween(scene02)
+        .setPin(".scene02")
+        .addIndicators({name: "Scene02"})
+        .addTo(controller);
 });
