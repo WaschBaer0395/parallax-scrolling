4ae9d597ba2739c2694e4336f6d325fbd5aa8b47
Aleyna Yildirimhan <aleynaydhn@gmail.com>
2019-06-29 09:58:16 +0200

    adjust text position scene13

--- javascript/main.js
+++ javascript/main.js
@@ -1465,7 +1465,7 @@ $(document).ready(function () {
 
         let scene13 = new ScrollMagic.Scene({
             triggerElement: '.scene13',
-            duration: 1000,
+            duration: 500,
             triggerHook: .6,
             offset: 0
         });
@@ -1491,49 +1491,49 @@ $(document).ready(function () {
 
     //START ANIMATION scene13
     let scene13Action = new TimelineLite();
-    scene13Action.to('#scene13_textblock1', 10, {autoAlpha: 1}, "+=2");
-    scene13Action.to('#scene13_textblock1', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_textblock1', 10, {autoAlpha: 1, y: 300, x: "10%"}, "+=4");
+    scene13Action.to('#scene13_textblock1', 15, {autoAlpha: 0, y: 400, x: "10%"}, "+=10");
     scene13Action.to('.blur', 5, {webkitFilter: 'blur(0px)', filter:'blur(0px)'}, "+=2");
     scene13Action.to('#scene13_layer01', 10, {autoAlpha: 0}, "+=1");
     scene13Action.to('#scene13_layer02', 5, {autoAlpha: 1}, "-=7");
 
-    scene13Action.to('#scene13_adambubble1', 10, {autoAlpha: 1}, "+=2");
-    scene13Action.to('#scene13_adamBubble1', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble1', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble1', 10, {autoAlpha: 0}, "-=10");
 
-    scene13Action.to('#scene13_adambubble2', 10, {autoAlpha: 1}, "+=2");
-    scene13Action.to('#scene13_adamBubble2', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble2', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble2', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble3', 10, {autoAlpha: 1}, "+=2");
-    scene13Action.to('#scene13_adamBubble3', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble3', 10, {autoAlpha: 1}, "+=1");
+    scene13Action.to('#scene13_adamBubble3', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble4', 10, {autoAlpha: 1}, "+=2");
-    scene13Action.to('#scene13_adamBubble4', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble4', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble4', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble5', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble5', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble5', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble5', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble6', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble6', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble6', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble6', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble7', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble7', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble7', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble7', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble8', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble8', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble8', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble8', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble9', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble9', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble9', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble9', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble10', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble10', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble10', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble10', 10, {autoAlpha: 0}, "+=10");
 
-    scene13Action.to('#scene13_adambubble11', 10, {autoAlpha: 1}, "+=10");
-    scene13Action.to('#scene13_adamBubble11', 10, {autoAlpha: 0}, "+=20");
+    scene13Action.to('#scene13_adambubble11', 10, {autoAlpha: 1}, "+=4");
+    scene13Action.to('#scene13_adamBubble11', 10, {autoAlpha: 0}, "+=10");
 
 
     const scene13 = new ScrollMagic.Scene({
         triggerElement: ".scene13",
-        duration: 30000,
+        duration: 40000,
         triggerHook: 0,
         reverse: true
     });
